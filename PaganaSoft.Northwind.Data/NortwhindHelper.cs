d9fbfd23b26ead4ef55689450dc381d137af1c2f
Abel Amaro Julian <amarotux@gmail.com>
2013-09-18 15:57:14 -0500

    Add methods to help the data acces

---
+++ PaganaSoft.Northwind.Data/NortwhindHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PaganaSoft.Northwind.Data.Models;
+
+namespace PaganaSoft.Northwind.Data
+{
+    public class NorthwindHelper
+    {
+        public IQueryable<Product> GetProductsByCategory(int CatId)
+        {
+            using (var db = new NorthwindContext())
+            {
+                return db.Products.Where(c => c.CategoryID == CatId);
+            }
+        }
+
+        public Product GetProductById(int ProdId)
+        {
+            using (var db = new NorthwindContext())
+            {
+                return db.Products.FirstOrDefault(p => p.ProductID == ProdId);
+            }
+        }
+
+    }
+}
